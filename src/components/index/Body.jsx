80972f7ecf969e2e2116c9b78aa0bbbc736deea7
wveimar.mamian@gmail.com <wveimar.mamian@gmail.com>
2022-03-01 14:37:35 -0500

    listo zindex

--- src/components/index/Body.jsx
+++ src/components/index/Body.jsx
@@ -3,24 +3,32 @@ import bragrund from '../../../public/image/004.png'
 import logo_large from '../../../public/image/002.png'
 import { Box } from '@mui/material'
 
+const style = (theme) => ({
+  zIndex: '2' , 
+  position:"absolute" ,
+  marginTop:"13rem" , 
+  maxWidth: "90rem",
+  [theme.breakpoints.down("sm")]: {
+    marginTop:"4rem" 
+  },
+});
+
 export const Body = () => {
   return (
     <>
-    <Box>
-        <Box sx={{ zIndex: 'tooltip' }}>
+    <Box >
+        <Box  sx={style}>
         <Image 
-    
     layout="intrinsic"
     src = {logo_large}
     alt="Picture of the author"
-  
     />
     </Box>
-   <Box sx={{ zIndex: 'modal',display:"block"}}>
+   <Box sx={{ zIndex: '1',display:"block"}}>
       <Image 
     layout="intrinsic"
     src = {bragrund}
-    alt="Picture of the author"
+    alt="color de fondo"
   
     />
     </Box>
