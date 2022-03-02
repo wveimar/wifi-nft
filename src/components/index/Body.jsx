b286988868aed74822aa56783ff9ba784477bee0
wveimar.mamian@gmail.com <wveimar.mamian@gmail.com>
2022-03-01 21:51:58 -0500

    responsive

--- src/components/index/Body.jsx
+++ src/components/index/Body.jsx
@@ -1,39 +1,76 @@
-import Image from 'next/image'
-import bragrund from '../../../public/image/004.png'
-import logo_large from '../../../public/image/002.png'
-import { Box } from '@mui/material'
+import Image from "next/image";
+import bragrund from "../../../public/image/004.png";
+import logo_large from "../../../public/image/002.png";
+import galery from "../../../public/image/007.png";
+import { Box, Stack, Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const style = (theme) => ({
-  zIndex: '2' , 
-  position:"absolute" ,
-  marginTop:"13rem" , 
-  maxWidth: "90rem",
+  zIndex: "2",
+  position: "absolute",
+  [theme.breakpoints.up('xl')]: {
+    marginTop: "18rem",
+    marginLeft: "8rem",
+    maxWidth: "80rem",
+  },
+ 
+  [theme.breakpoints.up('lg')]: {
+    marginTop: "15rem",
+    marginLeft: "7rem",
+    maxWidth: "60rem",
+  },
+  [theme.breakpoints.down('lg')]: {
+    marginTop: "11rem",
+    marginLeft: "6rem",
+    maxWidth: "47rem",
+  },
+  [theme.breakpoints.down('md')]: {
+    marginTop: "8rem",
+    marginLeft: "5.5rem",
+    maxWidth: "30rem",
+  },
   [theme.breakpoints.down("sm")]: {
-    marginTop:"4rem" 
+    marginTop: "5rem",
+    marginLeft: "2em",
+    maxWidth: "20rem",
   },
 });
 
 export const Body = () => {
   return (
     <>
-    <Box >
-        <Box  sx={style}>
-        <Image 
-    layout="intrinsic"
-    src = {logo_large}
-    alt="Picture of the author"
-    />
-    </Box>
-   <Box sx={{ zIndex: '1',display:"block"}}>
-      <Image 
-    layout="intrinsic"
-    src = {bragrund}
-    alt="color de fondo"
-  
-    />
-    </Box>
-
-    </Box>
+      <Box >
+        <Box sx={style}>
+          <Image
+            layout="intrinsic"
+            src={logo_large}
+            alt="Picture of the author"
+          />
+        </Box>
+        <Box sx={{ zIndex: "1", display: "block" }}>
+          <Image layout="intrinsic" src={bragrund} alt="color de fondo" />
+        </Box>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+          mt={6}
+          mb={2}
+        >
+          <Button
+            variant="contained"
+            endIcon={<SendIcon />}
+            sx={{ backgroundColor: "#91d8f6", width: "30%" }}
+          />
+          <Button
+            variant="contained"
+            endIcon={<SendIcon />}
+            sx={{ backgroundColor: "#91d8f6", width: "30%" }}
+          />
+        </Stack>
+        <Image layout="intrinsic" src={galery} alt="Picture of the author" />
+      </Box>
     </>
-  )
-}
+  );
+};
--- src/components/index/Header.jsx
+++ src/components/index/Header.jsx
@@ -1,46 +1,49 @@
-import Image from 'next/image'
-import logo_small from '../../../public/image/002.png'
-import { Box,Typography,AppBar,Toolbar } from '@mui/material'
-import {
-  createTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from '@mui/material/styles';
+import Image from "next/image";
+import logo_small from "../../../public/image/002.png";
+import { Box, Typography, AppBar, Toolbar } from "@mui/material";
 
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
+const style = (theme) => ({
+  flexGrow: "1",
+
+  [theme.breakpoints.up("xl")]: {},
+
+  [theme.breakpoints.up("lg")]: {
+    marginLeft: "7rem",
+    fontSize: "5rem",
+  },
+  [theme.breakpoints.down("lg")]: {
+    marginLeft: "5rem",
+    fontSize: "3.8rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    marginLeft: "0.5rem",
+    fontSize: "2.3rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    marginLeft: "0.3rem",
+    fontSize: "1.3rem",
+  },
+});
 
 export const Header = () => {
- 
   return (
     <>
-    <ThemeProvider theme={theme}>
-<Box alignItems="center">
-      <AppBar position="static" sx={{ backgroundColor:"#aaabaf"}} >
-        <Toolbar>
-        <Image 
-    src = {logo_small}
-    alt="Picture of the author"
-    width={300}
-    height={80}
+      <Box alignItems="center">
+        <AppBar position="static" sx={{ backgroundColor: "#aaabaf" }}>
+          <Toolbar>
+            <Image
+              src={logo_small}
+              alt="Picture of the author"
+              width={200}
+              height={40}
+            />
 
-    />
-    
-          <Typography 
-         variant="h2"
-          component="div" 
-          sx={{ flexGrow: 1 , ml:15}}
-        
-         
-          >
-            N F T ´ S Gallery
-          </Typography>
-
-        </Toolbar>
-      </AppBar>
-    </Box>
-
-    </ThemeProvider>
+            <Typography variant="h2" component="div" sx={style}>
+              N F T ´ S Gallery
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
-  )
-}
+  );
+};
