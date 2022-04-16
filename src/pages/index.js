0998531bc2d57b88e20a9ab02e361416483723dc
sebastian Hoyos Lopez <sebastiahoyos145@gmail.com>
2022-04-16 12:18:41 -0500

    se crea la carpeta layout y se estructura la componentes existentes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { Body } from '../components/index/Body'
-import { Header } from '../components/index/Header'
+import { Body } from "../components/Body/Body";
+import { Header } from "../components/Header/Header";
+import { Layout } from "../components/layout";
 
 export default function Home() {
   return (
-<>
-<Header/>
-<Body/>
-</>
-  )
+    <Layout>
+      <Body />
+    </Layout>
+  );
 }
