25d37daf967f6b73d269546ff1d7cc5f187a90a6
sebastian Hoyos Lopez <sebastiahoyos145@gmail.com>
2022-06-28 22:12:23 -0500

    se realizar el run build

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Bangers&family=Press+Start+2P&family=Quicksand:wght@500&family=Rubik+Bubbles&display=swap"
             rel="stylesheet"
